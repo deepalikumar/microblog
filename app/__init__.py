9e50c6374288d7ee64e40ad59015254e22985a7a
Deepali Kumar <deepaliprasad123@gmail.com>
2019-01-17 17:48:02 +0530

    added momemt flask to the application

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_moment import Moment
 
 
 
@@ -27,6 +28,7 @@ login = LoginManager(app)
 mail = Mail(app)
 login.login_view = 'login'
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 
 
