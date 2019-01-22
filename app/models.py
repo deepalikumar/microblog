b897f6eb302f22e6db2fa1d8bafd82f12d7cb2c3
Deepali Kumar <deepaliprasad123@gmail.com>
2019-01-22 22:40:17 +0530

    refactoring and introducing blueprint

--- app/models.py
+++ app/models.py
@@ -1,12 +1,12 @@
 from werkzeug.security import generate_password_hash, check_password_hash
 from app import login
-from app import db, app
+from app import db
 from flask_login import UserMixin
-
 from time import time
 import jwt
 from datetime import datetime
 from hashlib import md5
+from flask import current_app
 
 @login.user_loader
 def load_user(id):
@@ -67,11 +67,11 @@ class User(UserMixin, db.Model):
     def get_reset_password_token(self, expires_in=600):
         return jwt.encode(
             {'reset_password': self.id, 'exp': time() + expires_in},
-            app.config['SECRET_KEY'], algorithm='HS256').decode('utf-8')
+            current_app.config['SECRET_KEY'], algorithm='HS256').decode('utf-8')
     @staticmethod
     def verify_reset_password_token(token):
         try:
-            id = jwt.decode(token, app.config['SECRET_KEY'],
+            id = jwt.decode(token, current_app.config['SECRET_KEY'],
                 algorithms=['HS256'])['reset_password']
         except:
             return
