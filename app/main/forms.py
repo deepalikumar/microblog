858b8e17637f49886c4be54decdc06cec82b9401
Deepali Kumar <deepaliprasad123@gmail.com>
2019-02-03 23:01:45 +0530

    created a form to use the elasticsearch

--- app/main/forms.py
+++ app/main/forms.py
@@ -3,6 +3,7 @@ from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextA
 from wtforms.validators import DataRequired, ValidationError, EqualTo, Email, Length
 from app.models import User, Post
 from flask_babel import lazy_gettext as _l
+from flask import request
 
 
 class EditProfileForm(FlaskForm):
@@ -24,4 +25,14 @@ class PostForm(FlaskForm):
     post = TextAreaField(_l('Say Something'), validators=[
         DataRequired(), Length(min=1, max=140)])
     submit = SubmitField(_l('Submit'))
+
+class SearchForm(FlaskForm):
+    q = StringField(_l('Search', validators=[DataRequired()]))
+
+    def __init__(self, *args, **kwargs):
+        if 'formdata' not in kwargs:
+            kwargs['formdata'] = request.args
+        if 'csrf_enabled' not in kwargs:
+            kwargs['csrf_enabled'] = False
+        super(SearchForm, self).__init__(*args, **kwargs)
 
