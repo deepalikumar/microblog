c6c557ce18ec0fc95b835d51fff4788a493774f2
Deepali Kumar <deepaliprasad123@gmail.com>
2019-02-03 23:05:05 +0530

    creating a search route

--- app/main/routes.py
+++ app/main/routes.py
@@ -9,6 +9,8 @@ from flask_babel import _
 from flask import g, current_app
 from flask_babel import get_locale
 from app.main import bp
+from flask import g
+from app.main.forms import SearchForm
 
 @bp.route('/', methods=['GET', 'POST'])
 @bp.route('/index', methods=['GET', 'POST'])
@@ -52,6 +54,7 @@ def before_request():
     if current_user.is_authenticated:
         current_user.last_seen = datetime.utcnow()
         db.session.commit()
+        g.search_form = SearchForm()
     g.locale = str(get_locale())
 
 @bp.route('/edit_profile', methods=['GET', 'POST'])
@@ -114,4 +117,19 @@ def explore():
         if posts.has_prev else None
     return render_template('index.html', title="Explore", posts=posts.items, next_url=next_url, prev_url=prev_url)
 
+
+@bp.route('/search')
+@login_required
+def search():
+    if not g.search_form.validate():
+        return redirect(url_for('main.explore'))
+    page = request.args.get('page', 1, type=int)
+    posts, total = Post.search(g.search_form.q.data, page,
+                               current_app.config['POSTS_PER_PAGE'])
+    next_url = url_for('main.search', q=g.search_form.q.data, page=page + 1) \
+        if total > page * current_app.config['POSTS_PER_PAGE'] else None
+    prev_url = url_for('main.search', q=g.search_form.q.data, page=page - 1) \
+        if page > 1 else None
+    return render_template('search.html', title=_('Search'), posts=posts,
+                           next_url=next_url, prev_url=prev_url)
 
