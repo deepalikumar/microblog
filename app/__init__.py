75eed093fa9c809b196e5556ae28546be94d1f2e
deepalikumar <deepaliprasad123@gmail.com>
2019-03-12 11:22:08 +0530

    implemented logging

--- app/__init__.py
+++ app/__init__.py
@@ -10,6 +10,8 @@ from elasticsearch import Elasticsearch
 
 
 import logging
+from logging.handlers import RotatingFileHandler
+import os
 from logging.handlers import SMTPHandler
 from flask_migrate import Migrate
 from flask_login import LoginManager
@@ -74,6 +76,24 @@ def create_app(config_class=Config):
                 credentials=auth, secure=secure)
             mail_handler.setLevel(logging.ERROR)
             app.logger.addHandler(mail_handler)
+        
+        if app.config['LOG_TO_STDOUT']:
+            stream_handler = logging.StreamHandler()
+            stream_handler.setLevel(logging.INFO)
+            app.logger.addHandler(stream_handler)
+        else: 
+            if not os.path.exists('logs'):
+                os.mkdir('logs')
+            file_handler = RotatingFileHandler('logs/microblog.log',
+                                               maxBytes=10240, backupCount=10)
+            file_handler.setFormatter(logging.Formatter(
+                '%(asctime)s %(levelname)s: %(message)s '
+                '[in %(pathname)s:%(lineno)d]'))
+            file_handler.setLevel(logging.INFO)
+            app.logger.addHandler(file_handler)
+
+        app.logger.setLevel(logging.INFO)
+        app.logger.info('Microblog startup')
     
     return app
 
