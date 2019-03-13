5c797037a471eaf8d92bb00e143ad17a0f0d7889
deepalikumar <deepaliprasad123@gmail.com>
2019-03-13 10:25:22 +0530

    added notification route for the unread message notification

--- app/main/routes.py
+++ app/main/routes.py
@@ -2,7 +2,7 @@ from flask import render_template, redirect, flash , url_for, request
 from app import db
 from app.main.forms import  EditProfileForm, PostForm
 from flask_login import current_user, login_user, logout_user, login_required
-from app.models import User, Post, Message
+from app.models import User, Post, Message, Notification
 from werkzeug.urls import url_parse
 from datetime import datetime
 from flask_babel import _
@@ -146,6 +146,7 @@ def send_message(recipient):
     form = MessageForm()
     if form.validate_on_submit():
         msg = Message( author=current_user, recipient=user, body = form.message.data)
+        user.add_notification('unread_message_count', user.new_messages())
         db.session.add(msg)
         db.session.commit()
         flash(_('your message has been sent'))
@@ -157,6 +158,7 @@ def send_message(recipient):
 @login_required
 def messages(): 
     current_user.last_message_read_time = datetime.utcnow()
+    current_user.add_notification('unread_message_count', 0)
     db.session.commit()
     page = request.args.get('page', 1, type=int)
     messages = current_user.messages_received.order_by(
@@ -165,6 +167,19 @@ def messages():
             )
     next_url = url_for('main.messages', page=messages.next_num) if messages.has_next else None
     prev_url = url_for('main.messages', page=messages.prev_num) if messages.has_prev else None 
-
-    return render_template('messages.html', messages.messages.items, next_url=next_url, prev_url=prev_url)
     
+    return render_template('messages.html', messages=messages.items,
+                           next_url=next_url, prev_url=prev_url)
+
+
+@bp.route('/notifications')
+@login_required
+def notifications():
+    since = request.args.get('since', 0.0, type=float)
+    notifications = current_user.notifications.filter( Notification.timestamp > since ).order_by(Notification.timestamp.asc())
+
+    return jsonify([{
+        'name': n.name,
+        'data': n.get_data(),
+        'timestamp': n.timestamp
+    } for n in notifications ])
