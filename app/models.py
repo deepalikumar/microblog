66551b67415ef84bc84006e661c72e6d210a8fb2
deepalikumar <deepaliprasad123@gmail.com>
2019-03-13 10:26:08 +0530

    notification model

--- app/models.py
+++ app/models.py
@@ -129,6 +129,12 @@ class User(UserMixin, db.Model):
         last_read_time = self.last_message_read_time or datetime(1900, 1, 1)
         return Message.query.filter_by(recipient=self).filter(
             Message.timestamp > last_read_time).count()
+    
+    def add_notification(self, name, data):
+        self.notifications.filter_by(name=name).delete()
+        n = Notification(name=name, payload_json=json.dump(data), user=self)
+        db.session.add(n)
+        return n
         
 
 
@@ -153,3 +159,13 @@ class Message(db.Model):
 
     def __repr__(self):
         return '<Message {}>'.format(self.body)
+
+class Notification(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), index=True)
+    user_id= db.Column(db.Integer, db.ForeignKey('user_id'))
+    timestamp = db.Column(db.Float, index=True, default=time)
+    payload_json = db.Column(db.Text)
+
+    def get_data(self):
+        return json.loads(str(self.payload_json))
